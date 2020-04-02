dc19725a950452bb5edbf894a0e5894b908a110c
Clark Chen <chenhuajie1@gmail.com>
2020-04-02 15:40:30 -0400

    updates docker image version

--- build/run-in-docker.sh
+++ build/run-in-docker.sh
@@ -34,7 +34,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-E2E_IMAGE=sinceqq/nginx-amd64-e2e:v04022020-c04b32eb3
+E2E_IMAGE=sinceqq/nginx-amd64-e2e:v04022020-1a643b8cc
 
 DOCKER_OPTS=${DOCKER_OPTS:-}
 
